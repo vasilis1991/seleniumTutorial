a8d17672ed20c26b6bd1393c74cb9eebab4e4c5f
vasileios.ntokas <vasileio.ntokas@dwp.gsi.gov.uk>
2019-02-20 10:05:49 +0000

    add in first failure test

---
+++ src/test/java/uk/gov/bptds/seleniumTutorial/AmazonAddToBasketTest.java
@@ -0,0 +1,13 @@
+package uk.gov.bptds.seleniumTutorial;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class AmazonAddToBasketTest {
+
+    @Test
+    public void testSearchForProductAndAddToBasket(){
+        fail("Test Failure");
+    }
+}
